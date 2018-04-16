4b2998dc45adbdf912f95d89bb2e5aa25e4d3f9d
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-04-16 13:47:35 -0600

    Adding a POJO (Kotlin data class.. KDC? Kojo?) for the response

--- qtest-library/src/main/kotlin/io/paulbaker/qtest/Responses.kt
+++ qtest-library/src/main/kotlin/io/paulbaker/qtest/Responses.kt
@@ -1,7 +1,12 @@
 package io.paulbaker.qtest
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.time.LocalDateTime
+
+// Reference this for @JsonFormat patterns https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+const val PATTERN = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class User(
@@ -16,4 +21,19 @@ data class User(
         val avatar: String,
         @JsonProperty("ldap_username")
         val ldapUsername: String?
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Project(
+        val id: Long,
+        val name: String,
+        val description: String,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = PATTERN)
+        @JsonProperty("start_date")
+        val startDate: LocalDateTime?,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = PATTERN)
+        @JsonProperty("end_date")
+        val endDate: LocalDateTime?,
+        @JsonProperty("automation")
+        val automationEnabled: Boolean
 )
