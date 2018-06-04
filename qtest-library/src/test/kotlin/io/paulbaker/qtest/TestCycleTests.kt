d161e39a56977d8214ef1168ad9d230f1a95eae9
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-06-04 14:54:44 -0600

    Adding assertions

--- qtest-library/src/test/kotlin/io/paulbaker/qtest/TestCycleTests.kt
+++ qtest-library/src/test/kotlin/io/paulbaker/qtest/TestCycleTests.kt
@@ -3,8 +3,12 @@ package io.paulbaker.qtest
 import io.kotlintest.provided.getTestProject
 import io.kotlintest.provided.randomUUID
 import io.kotlintest.provided.testableQTestClient
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.not
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.notNullValue
+import org.hamcrest.Matchers.emptyOrNullString
+import org.hamcrest.Matchers.greaterThan
+import org.hamcrest.collection.IsEmptyCollection.empty
 import org.junit.jupiter.api.Test
 
 class TestCycleTests {
@@ -12,11 +16,37 @@ class TestCycleTests {
     private val testableQTestClient = testableQTestClient()
 
     @Test
+    fun testGetAllTestCycles() {
+        val testProject = getTestProject()
+        val releaseClient = testableQTestClient.releaseClient(testProject.id)
+        val releases = releaseClient.releases()
+        assertThat(releases, not(empty()))
+        releases.forEach { release ->
+            assertThat(release.id, greaterThan(0L))
+            assertThat(release.name, not(emptyOrNullString()))
+        }
+    }
+
+    @Test
+    fun testGetIndividualTestCycles() {
+        val testProject = getTestProject()
+        val releaseClient = testableQTestClient.releaseClient(testProject.id)
+        val releases = releaseClient.releases()
+        assertThat(releases, not(empty()))
+        releases.forEach { release ->
+            val releaseFromId = releaseClient.fromId(release.id)
+            assertThat(releaseFromId, `is`(release))
+        }
+    }
+
+    @Test
     fun testCreate() {
         val project = getTestProject()
         val testCycleClient = testableQTestClient.testCycleClient(project.id)
-        val createdTestCycle = testCycleClient.create(randomUUID())
-        assertThat(createdTestCycle, notNullValue())
+        val name = randomUUID()
+        val createdTestCycle = testCycleClient.create(name)
+        assertThat(createdTestCycle.id, greaterThan(0L))
+        assertThat(createdTestCycle.name, `is`(name))
         assert(testCycleClient.delete(createdTestCycle.id), { "Couldn't delete the test-cycle: ${createdTestCycle.name} - ${createdTestCycle.id}" })
         assert(!testCycleClient.delete(createdTestCycle.id), { "We shouldn't be able to delete the test-cycle twice: ${createdTestCycle.name} - ${createdTestCycle.id}" })
     }
