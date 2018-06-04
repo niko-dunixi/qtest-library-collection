84ffadf7a10364e0eb9324c43a269c4838cb91a1
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-06-04 15:28:03 -0600

    Adding user tests

--- qtest-library/src/test/kotlin/io/paulbaker/qtest/UserTests.kt
+++ qtest-library/src/test/kotlin/io/paulbaker/qtest/UserTests.kt
@@ -1,34 +1,31 @@
 package io.paulbaker.qtest
 
-//import io.kotlintest.inspectors.forAll
-//import io.kotlintest.matchers.collections.shouldNotBeEmpty
-//import io.kotlintest.matchers.gt
-//import io.kotlintest.provided.testableQTestClient
-//import io.kotlintest.shouldBe
-//import io.kotlintest.shouldNotBe
-//import io.kotlintest.specs.BehaviorSpec
-//
-//class UserTests : BehaviorSpec({
-//    Given("I have a project") {
-//        val testableQTestClient = testableQTestClient()
-//        val projectClient = testableQTestClient.projectClient()
-//        val project = projectClient.projects().first()
-//        val userClient = testableQTestClient.userClient()
-//        When("I request its list of users") {
-//            val usersFromProject = projectClient.users(project.id)
-//            Then("I get a full list of users") {
-//                usersFromProject.shouldNotBeEmpty()
-//                usersFromProject.forAll {
-//                    it shouldNotBe null
-//                    it.id shouldBe gt(0L)
-//                }
-//            }
-//            Then("It should be equivalent to requesting them individually") {
-//                usersFromProject.forEach({
-//                    val userFromId = userClient.fromId(it.id)
-//                    it shouldBe userFromId
-//                })
-//            }
-//        }
-//    }
-//})
+import io.kotlintest.provided.getTestProject
+import io.kotlintest.provided.testableQTestClient
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.greaterThan
+import org.hamcrest.collection.IsEmptyCollection.empty
+import org.hamcrest.text.IsEmptyString.emptyOrNullString
+import org.junit.jupiter.api.Test
+
+class UserTests {
+
+    @Test
+    fun testGetUsers() {
+        val testableQTestClient = testableQTestClient()
+        val projectId = getTestProject().id
+        val projectClient = testableQTestClient.projectClient()
+        val users = projectClient.users(projectId)
+        assertThat(users, not(empty()))
+
+        val userClient = testableQTestClient.userClient()
+        users.forEach { user ->
+            assertThat(user.id, greaterThan(0L))
+            assertThat(user.username, not(emptyOrNullString()))
+            val userFromId = userClient.fromId(user.id)
+            assertThat(userFromId, `is`(user))
+        }
+    }
+}
