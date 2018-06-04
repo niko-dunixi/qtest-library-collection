edaa3d9b63df573fa1ebb8858bcffee07c09ee22
Paul N. Baker <paul.nelson.baker@gmail.com>
2018-06-04 15:13:58 -0600

    Additional tests

--- qtest-library/src/test/kotlin/io/paulbaker/qtest/TestCycleTests.kt
+++ qtest-library/src/test/kotlin/io/paulbaker/qtest/TestCycleTests.kt
@@ -16,6 +16,45 @@ class TestCycleTests {
     private val testableQTestClient = testableQTestClient()
 
     @Test
+    fun testCreateRootTestCycles() {
+        val project = getTestProject()
+        val testCycleClient = testableQTestClient.testCycleClient(project.id)
+        val rootName = "${randomUUID()}-root-empty"
+        val rootTestCycle = testCycleClient.create(rootName)
+        assertThat(rootTestCycle.id, greaterThan(0L))
+        assertThat(rootTestCycle.name, `is`(rootName))
+    }
+
+    @Test
+    fun testCreateNestedTestCycles() {
+        val project = getTestProject()
+        val testCycleClient = testableQTestClient.testCycleClient(project.id)
+        val rootName = "${randomUUID()}-root-with-nested"
+        val rootTestCycle = testCycleClient.create(rootName)
+        assertThat(rootTestCycle.id, greaterThan(0L))
+        assertThat(rootTestCycle.name, `is`(rootName))
+
+        val nestedName = "${randomUUID()}-tc-nested"
+        val nestedTestCycle = testCycleClient.create(nestedName, TestCycleParent.TEST_CYCLE, rootTestCycle.id)
+        assertThat(nestedTestCycle.id, greaterThan(0L))
+        assertThat(nestedTestCycle.name, `is`(nestedName))
+
+        assertThat(rootTestCycle, `is`(not(nestedTestCycle)))
+    }
+
+    @Test
+    fun testDeleteTestCycles() {
+        val project = getTestProject()
+        val testCycleClient = testableQTestClient.testCycleClient(project.id)
+        val name = randomUUID()
+        val createdTestCycle = testCycleClient.create(name)
+        assertThat(createdTestCycle.id, greaterThan(0L))
+        assertThat(createdTestCycle.name, `is`(name))
+        assert(testCycleClient.delete(createdTestCycle.id), { "Couldn't delete the test-cycle: ${createdTestCycle.name} - ${createdTestCycle.id}" })
+        assert(!testCycleClient.delete(createdTestCycle.id), { "We shouldn't be able to delete the test-cycle twice: ${createdTestCycle.name} - ${createdTestCycle.id}" })
+    }
+
+    @Test
     fun testGetAllTestCycles() {
         val testProject = getTestProject()
         val releaseClient = testableQTestClient.releaseClient(testProject.id)
@@ -30,24 +69,12 @@ class TestCycleTests {
     @Test
     fun testGetIndividualTestCycles() {
         val testProject = getTestProject()
-        val releaseClient = testableQTestClient.releaseClient(testProject.id)
-        val releases = releaseClient.releases()
-        assertThat(releases, not(empty()))
-        releases.forEach { release ->
-            val releaseFromId = releaseClient.fromId(release.id)
+        val testCycleClient = testableQTestClient.testCycleClient(testProject.id)
+        val testCycles = testCycleClient.testCycles()
+        assertThat(testCycles, not(empty()))
+        testCycles.forEach { release ->
+            val releaseFromId = testCycleClient.fromId(release.id)
             assertThat(releaseFromId, `is`(release))
         }
-    }
-
-    @Test
-    fun testCreate() {
-        val project = getTestProject()
-        val testCycleClient = testableQTestClient.testCycleClient(project.id)
-        val name = randomUUID()
-        val createdTestCycle = testCycleClient.create(name)
-        assertThat(createdTestCycle.id, greaterThan(0L))
-        assertThat(createdTestCycle.name, `is`(name))
-        assert(testCycleClient.delete(createdTestCycle.id), { "Couldn't delete the test-cycle: ${createdTestCycle.name} - ${createdTestCycle.id}" })
-        assert(!testCycleClient.delete(createdTestCycle.id), { "We shouldn't be able to delete the test-cycle twice: ${createdTestCycle.name} - ${createdTestCycle.id}" })
     }
 }
